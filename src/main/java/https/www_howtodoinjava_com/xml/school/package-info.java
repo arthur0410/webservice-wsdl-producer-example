405eda27ff628c9dece934bbd89e0f70c117c5d8
Arthur Nakagawa de Oliveira <nakagawa0410@hotmail.com>
2020-02-13 23:34:31 -0300

    Initial commit

---
+++ src/main/java/https/www_howtodoinjava_com/xml/school/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2020.02.13 às 10:53:03 PM BRST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "https://www.howtodoinjava.com/xml/school", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package https.www_howtodoinjava_com.xml.school;
